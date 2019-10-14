bc9835e8bd97d0de79e13aa118997280c21bcb2d
Xavier Johanis <xavierjohanis@gmail.com>
2019-10-14 14:28:42 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding: 50}}>
+      <View style={{ flexDirection:'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder = "Course Goal"
+        style={{
+          width: '200', borderBottomColor: 'black', borderBottomWidth: 1
+        }} />
+        <Button title="ADD"/>
+        <View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
