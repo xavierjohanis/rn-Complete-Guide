fde3217df45138c99a912cb557a1dbbed2f27fde
Xavier Johanis <xavierjohanis@gmail.com>
2019-10-19 17:53:14 +0800

    components/props

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -27,11 +28,7 @@ export default function App() {
       <FlatList
         keyExtractor={(item, index) => item.id} 
         data = {courseGoals}
-        renderItem={itemData => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item.value}</Text>
-          </View>
-        )}
+        renderItem={itemData => ( <GoalItem title={itemData.item.value}/> )}
       />
     </View>
   );
@@ -51,12 +48,5 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 5
   }
 });
