91ef738ef05947bc7de8461dfd330878b47df2b2
Xavier Johanis <xavierjohanis@gmail.com>
2019-10-20 16:12:46 +0800

    delete goal

--- App.js
+++ App.js
@@ -10,13 +10,19 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle}]);
   };
 
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goalId !== goal.id);
+    });
+  }
+
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal = {addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id} 
         data = {courseGoals}
-        renderItem={itemData => ( <GoalItem title={itemData.item.value}/> )}
+        renderItem={itemData => ( <GoalItem onDelete={removeGoalHandler.bind(this, itemData.item.id)} title={itemData.item.value}/> )}
       />
     </View>
   );
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -15,22 +15,22 @@ const GoalInput = (props) => {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+        <Button title="ADD" onPress={(props.onAddGoal.bind(this, enteredGoal))} />
     </View>
     );
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-        },
-        input: {
-        width: '80%',
-        borderColor: 'black',
-        borderWidth: 1,
-        padding: 10
-        }
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+      },
+    input: {
+      width: '80%',
+      borderColor: 'black',
+      borderWidth: 1,
+      padding: 10
+      }
 })
 export default GoalInput;
